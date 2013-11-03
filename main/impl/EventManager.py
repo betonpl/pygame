3ed8419aca76893442799eba382a278c4585ea5e
Mateusz Odelga <m.odelga@vailantu.net>
2013-11-03 16:16:59 +0100

    Testing committing

--- main/impl/EventManager.py
+++ main/impl/EventManager.py
@@ -8,18 +8,18 @@ class EventManager(Tickable):
     HOVERABLE = 1
     CLICKABLE = 2
     __BROADCAST = -1.0, -1.0
-    
+
     class __EventListener():
         def __init__(self, key, delegate):
             self.key = key
             self.delegate = delegate
-        
+
     def __init__(self, game):
         Tickable.__init__(self, game, -1)
         self.__game = game
         self.listeners = {}
         self.listeners[EventManager.CLICKABLE] = []
-        self.listeners[EventManager.HOVERABLE] = [] 
+        self.listeners[EventManager.HOVERABLE] = []
 
     def registerAtEventIfAble(self, pos, manager, flags, eventType):
         if eventType & flags:
@@ -33,17 +33,17 @@ class EventManager(Tickable):
     def register(self, pos, manager, flags=CLICKABLE):
         self.registerAtEventIfAble(pos, manager, flags, eventType=EventManager.CLICKABLE)
         self.registerAtEventIfAble(pos, manager, flags, eventType=EventManager.HOVERABLE)
-    
+
     def __getListeners(self, event):
         return self.listeners[event]
-        
+
     def unregister(self, pos):
         pos = pos if pos != None else EventManager.__BROADCAST
         for eventType, listeners in self.listeners:
             for listener in listeners:
                 if self.__isBroadcastOrRegisteredService(pos, listener.key):
                     self.listeners[eventType].remove(listener)
-    
+
     def __checkClicks(self, p):
         pos = self.__translateToPos(p)
         for listener in self.__getListeners(EventManager.CLICKABLE):
@@ -55,25 +55,25 @@ class EventManager(Tickable):
         for listener in self.__getListeners(EventManager.HOVERABLE):
             if self.__isBroadcastOrRegisteredService(pos, listener.key):
                 listener.delegate.hover(pos)
-    
+
     def tick(self):
         for event in pygame.event.get():
             if event.type == QUIT or (event.type == KEYDOWN and event.key == K_ESCAPE):
-                return False 
+                return False
             if event.type == MOUSEBUTTONDOWN :
                 self.__checkClicks(pygame.mouse.get_pos())
             if event.type == MOUSEMOTION :
                 self.__checkHovers(pygame.mouse.get_pos())
         return True
-    
+
     @property
     def game(self):
         return self.__game
-    
+
     @staticmethod
     def __translateToPos(p):
         return floor(p[0] / Drawer.SQUARE_SIZE), floor(p[1] / Drawer.SQUARE_SIZE)
-  
+
     @staticmethod
     def __isBroadcastOrRegisteredService(pos, key):
         return key == pos or key == EventManager.__BROADCAST
